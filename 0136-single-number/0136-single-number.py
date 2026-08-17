994abff12c8f247a67ab086d3edb1fe11c003b82
Geetha Gandham <geethagandham07@gmail.com>
2026-08-17 09:29:05 +0530

    Time: 6 ms (38.44%), Space: 14.6 MB (15.97%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        freq={}
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+        for num in freq:
+            if freq[num]==1:
+                return num
+
+        
+        
